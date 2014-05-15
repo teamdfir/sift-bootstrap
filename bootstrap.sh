43524e61ff298365392b648733edec0017436828
Erik Kristensen <erik@erikkristensen.com>
2014-05-14 20:25:34 -0400

    adding new volatility plugin python requirements

--- bootstrap.sh
+++ bootstrap.sh
@@ -167,7 +167,7 @@ install_ubuntu() {
 }
 
 install_pip_packages() {
-    pip_packages="rekall docopt python-evtx python-registry"
+    pip_packages="rekall docopt python-evtx python-registry six construct"
 
     if [ "$@" = "dev" ]; then
         pip_packages="$pip_packages"
