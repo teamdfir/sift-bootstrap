6c0a46adf4a179b056e7ded11e52c6615ad71835
Erik Kristensen <erik@erikkristensen.com>
2017-06-17 08:50:56 -0600

    add the bootstrap file back, but with deprecation information

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Welcome to SIFT!"
+echo ""
+echo "Deprecation Notice: This script is no longer supported."
+echo ""
+echo "If you would like to install SIFT on Ubuntu 14.04 using this legacy script, please use the following url:"
+echo ""
+echo "   https://raw.githubusercontent.com/sans-dfir/sift-bootstrap/legacy/bootstrap.sh"
+echo ""
+echo "SIFT can now be installed on Ubuntu 16.04 using a new CLI tool called `sift`, this tool is available at"
+echo ""
+echo "   https://github.com/sans-dfir/sift-cli"
+echo ""
+echo "The SIFT CLI streamlines the installation process on 16.04, please see its readme file for download"
+echo "and usage instructions."
+echo ""
+echo ""
+
+exit 0
